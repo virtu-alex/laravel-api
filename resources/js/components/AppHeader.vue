3323508c2035d3039bead5a485ccbe0af4697727
virtu-alex <alessio.cordari@gmail.com>
2022-10-04 15:47:27 +0200

    feat: header

---
+++ resources/js/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="#">Navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <div class="navbar-nav">
+          <a class="nav-item nav-link active" href="#"
+            >Home <span class="sr-only">(current)</span></a>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+    name: "AppHeader",
+    props: {
+        title: String,
+    },
+};
+</script>
+<style lang="">
+
+</style>
