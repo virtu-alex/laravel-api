d18a9a478ee317dd85fba27c581a87696b091e90
virtu-alex <alessio.cordari@gmail.com>
2022-10-04 13:16:15 +0200

    first commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,12 @@
+<template lang="">
+    <div class="content">
+        <div class="title m-b-md">Work in progress</div>
+    </div>
+</template>
+<script>
+
+export default {
+    name: "App",
+};
+</script>
+<style lang=""></style>
