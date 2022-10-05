59561a62c3a5b2676e9b2dcf78fa89395af8ce48
virtu-alex <alessio.cordari@gmail.com>
2022-10-05 16:54:35 +0200

    feat: dynamic router-view

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,19 +1,18 @@
 <template>
     <div class="content">
         <AppHeader :title="title" />
-        <main class="container mt-3">Lista Post
-            <PostsList />
+        <main class="container mt-3">
+            <router-view></router-view>
         </main>
     </div>
 </template>
 <script>
 import AppHeader from "./AppHeader.vue";
-import PostsList from "./posts/PostsList.vue";
+
 export default {
     name: "App",
     components: {
         AppHeader,
-        PostsList,
     },
     data() {
         return {
